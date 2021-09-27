ad3edc59f6b7e6910b65f9a92bd0f2a47dd3af54
Wilfried PEPIN <wilfried.pepin35133@gmail.com>
2021-09-27 11:41:08 +0200

    second commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import demo.Money;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World !");
+        Money euros = new Money(42, "EUR");
+        System.out.println(euros);
+    }
+}
--- src/main/java/demo/Money.java
+++ src/main/java/demo/Money.java
@@ -1,5 +1,19 @@
 package demo;
 
 public class Money {
+    private int amount;
+    private String currency;
 
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "amount=" + amount +
+                ", currency='" + currency + '\'' +
+                '}';
+    }
 }
